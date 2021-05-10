1499552ebe500e7048d643b3c6b05f21051c2c65
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:11:55 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Module.php
+++ src/Module.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools-content-validation for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools-content-validation/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools-content-validation/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\ApiTools\ContentValidation;
 
 use Laminas\Mvc\MvcEvent;
